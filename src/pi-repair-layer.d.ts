deef37bcc519233814c11778383a95b9d4f18e56
henyojess <jessie@henyo.com>
2026-08-02 01:04:23 +0000

    chore(repair): remove old vendored submodule and clean up references

--- src/pi-repair-layer.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '#pi-repair-layer' {
-  import type { ExtensionAPI as _ExtensionAPI } from '@earendil-works/pi-coding-agent';
-
-  const toolRepair: (pi: _ExtensionAPI) => void;
-  export default toolRepair;
-}
